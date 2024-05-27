2305e2a6bad7a82181e6b5715c36b81e71de84f7
RwxMurphy <amurfie2003@gmail.com>
2024-05-27 03:51:27 -0400

    add user names to compromised_users file

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import csv
 
-compromised_users = []
+# Read csv
+compromised_users:str = []
 with open('passwords.csv', newline='') as password_file:
     password_csv = csv.DictReader(password_file)
     for psw_row in password_csv:
         compromised_users.append(psw_row['Username'])
 
-print(compromised_users)
+# Create txt file save comp_users
+with open('compromised_users.txt', 'w') as compromised_user_file:
+    for user in compromised_users:
+        compromised_user_file.write(user + '\n')
+    
